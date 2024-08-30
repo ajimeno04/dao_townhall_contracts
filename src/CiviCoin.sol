cffb1262f6ec844af6d202b8378c99f13d97b045
ajimeno04 <jimenobeltran11@gmail.com>
2024-08-30 09:15:59 +0200

    Fix: Update owner to msg.sender

--- src/CiviCoin.sol
+++ src/CiviCoin.sol
@@ -4,33 +4,11 @@ pragma solidity ^0.8.10;
 import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract CivicCoin is ERC20, Ownable {
+contract CiviCoin is ERC20, Ownable {
 
-    constructor() ERC20("CivicCoin", "CVC") {}
+    constructor() ERC20("CiviCoin", "CVC") Ownable(msg.sender) {}
 
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
-    }
-}
-
-contract TownHallDAO is Ownable {
-    CivicCoin public civicCoin;
-
-    mapping(address => bool) public isUser;
-
-    event UserAdded(address indexed user, uint256 amount);
-
-    constructor(address _civicCoinAddress) {
-        civicCoin = CivicCoin(_civicCoinAddress);
-    }
-
-    function addUser(address user, uint256 amount) external onlyOwner {
-        require(!isUser[user], "User already exists");
-
-        civicCoin.mint(user, amount);
-
-        isUser[user] = true;
-
-        emit UserAdded(user, amount);
     }
 }
